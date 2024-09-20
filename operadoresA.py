26f60133bbb56a24ba61eb5b46fbd06f62f8b057
Roberto Vazquez <a.22308051281326@cbtis128.edu.mx>
2024-09-20 09:33:12 -0600

    Add files via upload

---
+++ operadoresA.py
@@ -0,0 +1,46 @@
+print("Act 9 clases v2")
+print("Roberto Vazquez Mat:22308051281326")
+#Zona de Class
+
+class Operadores1326:
+    #Zona de funciones
+    def suma1326(self,E,N):
+        s1326=E+N
+        print(f"La suma de {E} + {N} = {s1326}")
+    def resta1326(self,E,N):
+        r1326=E-N
+        print(f"La resta de {E} - {N} = {r1326}")
+    def mul1326(self,E,N):
+        m1326=E*N
+        print(f"La multiplicacion de {E} * {N} = {m1326}")
+    def div1326(self,E,N):
+        d1326=E/N
+        print(f"La division de {E} / {N} = {d1326}")
+    def mod1326(self,E,N):
+        mo1326=E%N
+        print(f"El modulo de {E} % {N} = {mo1326}")
+    def exp1326(self,E,N):
+        e1326=E**N
+        print(f"La exponente de {E} ^ {N} = {e1326}")
+    def coci1326(self,E,N):
+        c1326=E//N
+        print(f"El cociente de {E} // {N} = {c1326}")
+
+# Zona de creacion del objeto
+operavaz=Operadores1326()
+
+#Zona de uso de objetos
+print(" Funcion Suma")
+operavaz.suma1326(5,4)
+print(" Funcion Resta")
+operavaz.resta1326(5,4)
+print(" Funcion Multiplicación")
+operavaz.mul1326(5,4)
+print(" Funcion División")
+operavaz.div1326(5,)
+print(" Funcion Modulo")
+operavaz.mod1326(5,4)
+print(" Funcion Exponente")
+operavaz.exp1326(5,4)
+print(" Funcion Cociente")
+operavaz.coci1326(5,4)
